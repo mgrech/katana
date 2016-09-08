b79531c910d258fb0f4109857e23b4143402695a
Markus Grech <me@mgrech.dev>
2016-09-08 11:15:24 +0200

    added static_asserts to katana.h + better formatting

--- katana/src/katana.h
+++ katana/src/katana.h
@@ -50,26 +50,47 @@ typedef bool kbool;
 typedef _Bool kbool;
 #endif
 
-typedef signed char kint8;
-typedef unsigned char kuint8;
-typedef short kint16;
-typedef unsigned short kuint16;
-typedef int kint32;
-typedef unsigned kuint32;
-typedef long long kint64;
+typedef signed char        kint8;
+typedef unsigned char      kuint8;
+typedef short              kint16;
+typedef unsigned short     kuint16;
+typedef int                kint32;
+typedef unsigned           kuint32;
+typedef long long          kint64;
 typedef unsigned long long kuint64;
-typedef float kfloat32;
-typedef double kfloat64;
-typedef void* kptr;
+typedef float              kfloat32;
+typedef double             kfloat64;
+typedef void*              kptr;
 
 #if defined(KATANA_ARCH_AMD64)
-typedef kint64 kint;
+typedef kint64  kint;
 typedef kuint64 kuint;
-typedef kint64 kpint;
+typedef kint64  kpint;
 typedef kuint64 kupint;
 #elif defined(KATANA_ARCH_X86)
-typedef kint32 kint;
+typedef kint32  kint;
 typedef kuint32 kuint;
-typedef kint32 kpint;
+typedef kint32  kpint;
 typedef kuint32 kupint;
 #endif
+
+#ifdef __cplusplus
+#define KSTATIC_ASSERT(expr) static_assert(expr, #expr);
+#else
+#define KSTATIC_ASSERT(expr) _Static_assert(expr, #expr);
+#endif
+
+KSTATIC_ASSERT(sizeof(kbool) == 1)
+
+KSTATIC_ASSERT(sizeof(kint8)  == 1 && sizeof(kuint8)  == 1)
+KSTATIC_ASSERT(sizeof(kint16) == 2 && sizeof(kuint16) == 2)
+KSTATIC_ASSERT(sizeof(kint32) == 4 && sizeof(kuint32) == 4)
+KSTATIC_ASSERT(sizeof(kint64) == 8 && sizeof(kuint64) == 8)
+
+KSTATIC_ASSERT(sizeof(kpint) == sizeof(void*) && sizeof(kupint) == sizeof(void*))
+KSTATIC_ASSERT(sizeof(void*) == sizeof(void(*)()))
+
+KSTATIC_ASSERT(sizeof(kfloat32) == 4)
+KSTATIC_ASSERT(sizeof(kfloat64) == 8)
+
+#undef KSTATIC_ASSERT
