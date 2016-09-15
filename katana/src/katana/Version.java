0646cb9744e4e8b86fee634293b1c0b30686afc6
Markus Grech <me@mgrech.dev>
2016-09-15 14:35:26 +0200

    bump version number

--- katana/src/katana/Version.java
+++ katana/src/katana/Version.java
@@ -17,7 +17,7 @@ package katana;
 public class Version
 {
 	public static final int MAJOR = 0;
-	public static final int MINOR = 2;
+	public static final int MINOR = 3;
 
 	public static final String AUTHOR = "Markus Grech";
 
