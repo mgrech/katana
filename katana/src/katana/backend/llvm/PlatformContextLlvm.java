ea18420ebd3b3a19cbf257237614ee2c5bf8788e
Markus Grech <me@mgrech.dev>
2016-10-09 09:26:06 +0200

    move core builtins to top-level namespace, remove ashr and rename shrl to shr

--- katana/src/katana/backend/llvm/PlatformContextLlvm.java
+++ katana/src/katana/backend/llvm/PlatformContextLlvm.java
@@ -38,29 +38,28 @@ public abstract class PlatformContextLlvm implements PlatformContext
 
 	static
 	{
-		registerBinaryOp("katana.eq",   "icmp eq", "icmp eq",  "icmp eq",  "fcmp oeq", "icmp eq", SemaTypeBuiltin.BOOL);
-		registerBinaryOp("katana.neq",  "icmp ne", "icmp ne",  "icmp ne",  "fcmp one", "icmp ne", SemaTypeBuiltin.BOOL);
-		registerBinaryOp("katana.lt",   null,      "icmp slt", "icmp ult", "fcmp olt", null,      SemaTypeBuiltin.BOOL);
-		registerBinaryOp("katana.lteq", null,      "icmp sle", "icmp ule", "fcmp ole", null,      SemaTypeBuiltin.BOOL);
-		registerBinaryOp("katana.gt",   null,      "icmp sgt", "icmp ugt", "fcmp ogt", null,      SemaTypeBuiltin.BOOL);
-		registerBinaryOp("katana.gteq", null,      "icmp sge", "icmp uge", "fcmp oge", null,      SemaTypeBuiltin.BOOL);
+		registerBinaryOp("eq",   "icmp eq", "icmp eq",  "icmp eq",  "fcmp oeq", "icmp eq", SemaTypeBuiltin.BOOL);
+		registerBinaryOp("neq",  "icmp ne", "icmp ne",  "icmp ne",  "fcmp one", "icmp ne", SemaTypeBuiltin.BOOL);
+		registerBinaryOp("lt",   null,      "icmp slt", "icmp ult", "fcmp olt", null,      SemaTypeBuiltin.BOOL);
+		registerBinaryOp("lteq", null,      "icmp sle", "icmp ule", "fcmp ole", null,      SemaTypeBuiltin.BOOL);
+		registerBinaryOp("gt",   null,      "icmp sgt", "icmp ugt", "fcmp ogt", null,      SemaTypeBuiltin.BOOL);
+		registerBinaryOp("gteq", null,      "icmp sge", "icmp uge", "fcmp oge", null,      SemaTypeBuiltin.BOOL);
 
-		registerBinaryOp("katana.add", null, "add",  "add",  "fadd", null, null);
-		registerBinaryOp("katana.sub", null, "sub",  "sub",  "fsub", null, null);
-		registerBinaryOp("katana.mul", null, "mul",  "mul",  "fmul", null, null);
-		registerBinaryOp("katana.div", null, "sdiv", "udiv", "fdiv", null, null);
-		registerBinaryOp("katana.mod", null, "srem", "urem", "frem", null, null);
+		registerBinaryOp("add", null, "add",  "add",  "fadd", null, null);
+		registerBinaryOp("sub", null, "sub",  "sub",  "fsub", null, null);
+		registerBinaryOp("mul", null, "mul",  "mul",  "fmul", null, null);
+		registerBinaryOp("div", null, "sdiv", "udiv", "fdiv", null, null);
+		registerBinaryOp("mod", null, "srem", "urem", "frem", null, null);
 
-		registerBinaryOp("katana.shl",  null,  "shl",  "shl",  null, null, null);
-		registerBinaryOp("katana.shrl", null,  "lshr", "lshr", null, null, null);
-		registerBinaryOp("katana.shra", null,  "ashr", "ashr", null, null, null);
-		registerBinaryOp("katana.and",  "and", "and",  "and",  null, null, null);
-		registerBinaryOp("katana.or",   "or",  "or",   "or",   null, null, null);
-		registerBinaryOp("katana.xor",  "xor", "xor",  "xor",  null, null, null);
+		registerBinaryOp("shl", null,  "shl",  "shl",  null, null, null);
+		registerBinaryOp("shr", null,  "lshr", "lshr", null, null, null);
+		registerBinaryOp("and", "and", "and",  "and",  null, null, null);
+		registerBinaryOp("or",  "or",  "or",   "or",   null, null, null);
+		registerBinaryOp("xor", "xor", "xor",  "xor",  null, null, null);
 
-		registerPointerIntegerConversion("katana.ptrtou", PointerIntegerConversion.Which.PTR2UPINT);
-		registerPointerIntegerConversion("katana.ptrtoi", PointerIntegerConversion.Which.PTR2PINT);
-		registerPointerIntegerConversion("katana.inttoptr", PointerIntegerConversion.Which.INT2PTR);
+		registerPointerIntegerConversion("ptrtou", PointerIntegerConversion.Which.PTR2UPINT);
+		registerPointerIntegerConversion("ptrtoi", PointerIntegerConversion.Which.PTR2PINT);
+		registerPointerIntegerConversion("inttoptr", PointerIntegerConversion.Which.INT2PTR);
 	}
 
 	@Override
