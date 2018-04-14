0365649156a6e9ad8685b97db84a241a090e34ca
Markus Grech <me@mgrech.dev>
2018-04-14 19:43:46 +0200

    add new test for rejecting non-ascii characters to MiscellaneousTests

--- compiler/src/test/io/katana/compiler/testing/scanner/tests/MiscellaneousTests.java
+++ compiler/src/test/io/katana/compiler/testing/scanner/tests/MiscellaneousTests.java
@@ -1,5 +1,8 @@
 package io.katana.compiler.testing.scanner.tests;
 
+import io.katana.compiler.scanner.ScannerDiagnostics;
+import io.katana.compiler.scanner.TokenCategory;
+import io.katana.compiler.scanner.TokenType;
 import io.katana.compiler.testing.scanner.Tokenization;
 import org.junit.Test;
 
@@ -9,6 +12,16 @@ public class MiscellaneousTests
 	public void acceptsEmptyFile()
 	{
 		var tok = Tokenization.of("");
+		tok.expectNoFurtherTokensOrErrors();
+	}
+
+	@Test
+	public void rejectsInvalidCodepoints()
+	{
+		var tok = Tokenization.of("\fabc\u1234");
+		tok.expectError(0, ScannerDiagnostics.INVALID_CODEPOINT, 1);
+		tok.expectError(4, ScannerDiagnostics.INVALID_CODEPOINT, 1);
+		tok.expectToken(1, TokenCategory.IDENT, TokenType.IDENT, "abc");
 		tok.expectNoFurtherTokensOrErrors();
 	}
 }
